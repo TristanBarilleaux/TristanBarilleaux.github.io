5a41cae1ce7b0106ce5f3558ed5ae3a954d368e5
TristanBarilleaux <tjb0549988@lpssonline.com>
2022-10-10 18:10:36 +0000

    added platforms, collectables, and cannon

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -12,9 +12,11 @@
    *
    * Use the createPlatform Function to create platforms for the level.
    *
-   * createPlatform() takes these arguments:
+   * 
    *
-   *      createPlatform(x, y, scaleX, scaleY);
+   *       
+   *      
+   *
    *
    *      x: The x coordineate for the platform.
    *      y: The y coordineate for the platform.
@@ -28,18 +30,11 @@
 
     ////////////////////////////////////////////////////////////////////////
     // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
-
-    /*
-     * ground : here, we create a floor. Given the width of of the platform
-     * asset, giving it a scaleX and scaleY of 2 will stretch it across the
-     * bottom of the game.
-     */
-    createPlatform(0, game.world.height - 32, 3, 2); // DO NOT DELETE
-
-    // example:
-    createPlatform(400, 250);
-    createPlatform(170, 460, 0.5);
-
+    createPlatform(400, 250);           
+    createPlatform(150, 370, 0.5);       
+    createPlatform(250, 500, 0.9,);   
+    createPlatform(720, 560, 0.8);
+   
     // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////
   }
